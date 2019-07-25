12c99fcfe1205c12daef2b578338676688ae9f5b
Lina Ochoa <lina.m8a@gmail.com>
2019-07-25 15:33:10 +0200

    Adding descriptive Jupyter notebook about Quaatlas

---
+++ stract/stract/stract.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as mplot
+
+# Load Quaatlas matrix and create dataframe
+quaatlas = pd.read_csv('./../data/quaatlas/matrix.csv')
