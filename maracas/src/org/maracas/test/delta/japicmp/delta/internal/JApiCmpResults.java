fc5494cf82f144150f159558224394fdbcfd25ea
Lina Ochoa <lina.m8a@gmail.com>
2020-03-05 10:39:32 +0100

    Fixing delta tests

--- maracas/src/org/maracas/test/delta/japicmp/delta/internal/JApiCmpResults.java
+++ maracas/src/org/maracas/test/delta/japicmp/delta/internal/JApiCmpResults.java
@@ -86,7 +86,7 @@ public class JApiCmpResults {
 	
 	private List<JApiClass> compare(String apiOld, String apiNew, IEvaluatorContext eval) {
 		Options defaultOptions = Options.newDefault();
-		defaultOptions.setAccessModifier(AccessModifier.PACKAGE_PROTECTED);
+		defaultOptions.setAccessModifier(AccessModifier.PROTECTED);
 		defaultOptions.setOutputOnlyModifications(true);
 		defaultOptions.setClassPathMode(ClassPathMode.TWO_SEPARATE_CLASSPATHS);
 		defaultOptions.setIgnoreMissingClasses(true);
