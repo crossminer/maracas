b12db69d3e9352492d07f4ac24bc429ef435d2fe
Lina Ochoa <lina.m8a@gmail.com>
2021-01-04 20:05:20 +0100

    Adding parent pom

---
+++ maracas-parent/src/main/java/org/swat/maracas/maracas_parent/App.java
@@ -0,0 +1,13 @@
+package org.swat.maracas.maracas_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
