c7cccdcb180c765bd3f2c25bc6ddc41feabcce1c
Lina Ochoa <lina.m8a@gmail.com>
2021-05-31 17:04:35 +0200

    Addiing methodMoreAccessible client migration

---
+++ data/comp-changes-client-new/src/mainclient/methodMoreAccessible/MethodMoreAccessibleExt.java
@@ -0,0 +1,18 @@
+package mainclient.methodMoreAccessible;
+
+import main.methodMoreAccessible.MethodMoreAccessible;
+
+/**
+ * Reactions:
+ * - Change method visibility from protected to public
+ * protected2public()
+ * @author Lina Ochoa
+ *
+ */
+public class MethodMoreAccessibleExt extends MethodMoreAccessible {
+
+	@Override
+	public void protected2public() {
+		
+	}
+}
