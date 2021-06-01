e3efc43198fd318c4aceb2a5aafca0b69045d62f
Lina Ochoa <lina.m8a@gmail.com>
2021-06-01 09:03:26 +0200

    Addiing methodNowFinal client migration

---
+++ data/comp-changes-client-new/src/mainclient/methodNowFinal/MethodNowFinalExt.java
@@ -0,0 +1,17 @@
+package mainclient.methodNowFinal;
+
+import main.methodNowFinal.MethodNowFinal;
+
+/**
+ * Reactions:
+ * - Remove methodNowFinal() implementation
+ * - Remove sMethodNowFinal() implementation
+ * @author Lina Ochoa
+ *
+ */
+public class MethodNowFinalExt extends MethodNowFinal {
+	
+	public int methodNowFinalClient() {
+		return super.methodNowFinal();
+	}
+}
